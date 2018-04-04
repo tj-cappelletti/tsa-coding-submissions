c49d5b807fe8d371cc74b042cff542d865222312
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2018-04-04 17:16:44 -0400

    Added seed for roles

---
+++ Tsa.CodingChallenge.Submissions.Database/Scripts/SeedData/dbo.Roles.Data.sql
@@ -0,0 +1,16 @@
+﻿SET IDENTITY_INSERT dbo.Roles ON
+
+MERGE dbo.Roles AS [target]
+USING ( VALUES (1, 'JUDGE', 'Judge', NULL),
+               (2, 'TEAM_PARTICIPANT', 'Team Participant', NULL))
+               AS [source] (Id, Identifier, [Name], [Description])
+ON [target].Id = [source].Id
+WHEN MATCHED AND [target].Identifier != [source].Identifier
+			 OR  [target].[Name] != [source].[Name]
+			 OR  [target].[Description] != [source].[Description]
+	THEN UPDATE SET	[target].Identifier = [source].Identifier,
+					[target].[Name] = [source].[Name],
+					[target].[Description] = [source].[Description]
+WHEN NOT MATCHED
+	THEN	INSERT (Id, Identifier, [Name], [Description])
+			VALUES (Id, Identifier, [Name], [Description]);
