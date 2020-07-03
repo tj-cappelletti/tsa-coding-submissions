561a6b482e13b0fb33e93ce11268e18432499382
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2020-07-03 19:26:37 -0400

    Wiring API up
    Added Docker support
    Added SQL Server connection for entities

--- Tsa.CodingChallenge.Submissions.WebApi/Startup.cs
+++ Tsa.CodingChallenge.Submissions.WebApi/Startup.cs
@@ -1,40 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Tsa.CodingChallenge.Submissions.Core.DataContexts;
 
 namespace Tsa.CodingChallenge.Submissions.WebApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
 
@@ -42,10 +27,13 @@ namespace Tsa.CodingChallenge.Submissions.WebApi
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SubmissionsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SubmissionsContext")));
+            services.AddControllers();
         }
     }
 }
