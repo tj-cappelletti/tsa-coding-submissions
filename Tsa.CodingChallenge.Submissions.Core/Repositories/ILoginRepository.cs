d5175f392004a151edf411b1fd17b7481c12e6b7
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2020-07-03 19:08:30 -0400

    Added login repository

---
+++ Tsa.CodingChallenge.Submissions.Core/Repositories/ILoginRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tsa.CodingChallenge.Submissions.Core.Entities;
+
+namespace Tsa.CodingChallenge.Submissions.Core.Repositories
+{
+    public interface ILoginRepository : IRepositoryModifier<Login>,
+        IRepositoryModifierAsync<Login>,
+        IRepositoryReader<Login>,
+        IRepositoryReaderAsync<Login> { }
+}
---
+++ Tsa.CodingChallenge.Submissions.Core/Repositories/IRepositoryReaderAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Tsa.CodingChallenge.Submissions.Core.Repositories
+{
+    public interface IRepositoryReaderAsync<TEntity>
+    {
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<IEnumerable<TEntity>> GetAsync();
+
+        Task<TEntity> GetAsync(int id);
+
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<IEnumerable<TEntity>> WhereAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
