60fa242c967f0f9bcf9a373060e8c550781c6d92
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2020-07-03 19:25:50 -0400

    Started migrating static content
    Got the layout back to what it was as opposed to Blazor OOB
    Moved some of the static content back from the previous site in archive

--- Tsa.CodingChallenge.Submissions.Blazor/Startup.cs
+++ Tsa.CodingChallenge.Submissions.Blazor/Startup.cs
@@ -1,34 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Tsa.CodingChallenge.Submissions.Blazor.Data;
 
 namespace Tsa.CodingChallenge.Submissions.Blazor
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +39,14 @@ namespace Tsa.CodingChallenge.Submissions.Blazor
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
         }
     }
 }
