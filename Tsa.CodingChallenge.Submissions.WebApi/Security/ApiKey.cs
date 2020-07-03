042971f078d14b7d0fc0d43a32f6ec86f3453c1d
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2020-07-03 14:46:14 -0400

    Created new API project
    Stubbed out without any functionality

---
+++ Tsa.CodingChallenge.Submissions.WebApi/Security/ApiKey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tsa.CodingChallenge.Submissions.WebApi.Security
+{
+    public class ApiKey
+    {
+        public DateTime Created { get; }
+
+        public int Id { get; }
+
+        public string Key { get; }
+
+        public string Owner { get; }
+
+        public IReadOnlyCollection<string> Roles { get; }
+
+        public ApiKey(int id, string owner, string key, DateTime created, IReadOnlyCollection<string> roles)
+        {
+            Id = id;
+            Owner = owner ?? throw new ArgumentNullException(nameof(owner));
+            Key = key ?? throw new ArgumentNullException(nameof(key));
+            Created = created;
+            Roles = roles ?? throw new ArgumentNullException(nameof(roles));
+        }
+    }
+}
