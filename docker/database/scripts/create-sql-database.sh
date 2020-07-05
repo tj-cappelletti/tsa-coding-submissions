bd052eb1bd7a0a7cf4229dfc8425242ff2d5aee2
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2020-07-04 22:30:05 -0400

    Fixedup post move
    Cleaned up create-sql-database.sh to work safer and more informative
    Updated Dockerfile to work with moving items around

--- docker/database/scripts/create-sql-database.sh
+++ docker/database/scripts/create-sql-database.sh
@@ -1,35 +1,41 @@
-/opt/mssql/bin/sqlservr &
+#!/bin/sh
 
-SQL_PID=$!
+# Launch SQL Server as a background process
+# Direct stdout to null but allow errors to be written to the console
+/opt/mssql/bin/sqlservr > /dev/null &
 
+# Variables to check if SQL Server starts in a timely manner
 sqlCheckCount=0
-sqlIsAlive=false
+sqlIsAlive=0
 
+# Loop to wait for SQL Server to startup
 while [ $sqlCheckCount -lt 4 ]
 do
 	echo "Waiting for SQL Server to start..."
-	sleep 15
+	sleep 5
 
+	# Try a command that will tell us if SQL Server is running
+	# Dump all output to null, we expect some errors while starting
 	echo "Checking if SQL Server has started..."
-	/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "SELECT * FROM sys.dabases"
+	/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "SELECT TOP 1 * FROM sys.dabases" > /dev/null 2>&1
 
+	# Break loop if the query was sucessful
 	if [ $? -eq 0 ]
 	then
 		sqlCheckCount=5
-		sqlIsAlive=true
+		sqlIsAlive=1
 	else
 		sqlCheckCount=$(( $sqlCheckCount + 1))
 	fi
 done
 
-if [ "$sqlIsAlive" == false ]
+# If SQL Server didn't start, return a non-successful code
+if [ $sqlIsAlive -eq 0 ]
 then
 	echo "SQL Server failed to start in a timely manner..."
 	exit 1
 fi
 
+# Run the script to create the database
 echo "SQL Server is running, creating database..."
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i /create-db.sql
-
-echo "Shutting down SQL Server..."
-kill $SQL_PID
