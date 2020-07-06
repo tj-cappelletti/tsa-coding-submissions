64ca78b54fc47d0679c671b8c74375985a2f74e2
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2020-07-06 08:28:21 -0400

    Formatting changes

--- Tsa.CodingChallenge.Submissions.Core/Entities/TeamMember.cs
+++ Tsa.CodingChallenge.Submissions.Core/Entities/TeamMember.cs
@@ -3,8 +3,11 @@
     public class TeamMember
     {
         public int Id { get; set; }
+
         public int LoginId { get; set; }
+
         public Login Login { get; set; }
+
         public string MemberId { get; set; }
     }
 }
